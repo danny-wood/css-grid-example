acf909454c03a72066d2d12e0f10e626041042c0
danny-wood <dannywood1204@gmail.com>
2020-12-27 21:52:08 +0000

    Remove default files

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main>
+      <header>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+        </nav>
+      </header>
+      <section>
+        <h1>Home Page</h1>
+      </section>
+    </main>
+  );
+}
+
+export default App;
